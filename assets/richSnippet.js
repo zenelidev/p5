3cc2268b00d6778b08e99ae41d172511d07bea67
unknown <sarah.sarahlea1998@gmail.com>
2023-08-18 13:13:34 +0200

    update

---
+++ assets/richSnippet.js
@@ -0,0 +1,30 @@
+
+var richSnippetData = {
+    "@context": "http://schema.org",
+    "@type": "Person",
+    "name": "Nina Carducci",
+    "jobTitle": "Photographe",
+    "url": "https://nina-carducci.github.io/",
+    "image": "URL de l'image du client",
+    "description": "Portfolio de photographe : Devenir photographe était pour moi une évidence. Comme si j’y étais prédestiné. Saisir un moment, une émotion, une situation, un endroit, une lumière et les rendre immortels, voilà ce qui me fait vibrer.",
+    "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "68 avenue Alsace-Lorraine",
+        "addressLocality": "Bordeaux",
+        "postalCode": "33200",
+        "addressCountry": "France"
+    },
+    "telephone": "05 56 67 78 89",
+    "availability": {
+        "@type": "OpeningHoursSpecification",
+        "opens": "10:00",
+        "closes": "19:00",
+        "dayOfWeek": [
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday"
+        ]
+    }
+};
