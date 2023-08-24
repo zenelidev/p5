d5568d416b240247794725a0d848e2e123c3de30
unknown <sarah.sarahlea1998@gmail.com>
2023-08-24 23:45:40 +0200

    modifications

--- assets/richSnippet.js
+++ assets/richSnippet.js
@@ -5,7 +5,7 @@ var richSnippetData = {
     "name": "Nina Carducci",
     "jobTitle": "Photographe",
     "url": "https://nina-carducci.github.io/",
-    "image": "URL de l'image du client",
+    "image": "https://nina-carducci.github.io/assets/images/nina.webp",
     "description": "Portfolio de photographe : Devenir photographe était pour moi une évidence. Comme si j’y étais prédestiné. Saisir un moment, une émotion, une situation, un endroit, une lumière et les rendre immortels, voilà ce qui me fait vibrer.",
     "address": {
         "@type": "PostalAddress",
